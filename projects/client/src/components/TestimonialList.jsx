6ebabe228fc5953ff1dc6ae5d6925f67703d33ff
budi760 <budisetiawan760@gmail.com>
2023-03-13 01:22:10 +0700

    fix over testimony height

--- projects/client/src/components/TestimonialList.jsx
+++ projects/client/src/components/TestimonialList.jsx
@@ -21,24 +21,7 @@ const TestimonialContent = (props) => {
       boxShadow={'lg'}
       p={8}
       rounded={'xl'}
-      align={'center'}
-      pos={'relative'}
-      _after={{
-        content: `""`,
-        w: 0,
-        h: 0,
-        borderLeft: 'solid transparent',
-        borderLeftWidth: 16,
-        borderRight: 'solid transparent',
-        borderRightWidth: 16,
-        borderTop: 'solid',
-        borderTopWidth: 16,
-        borderTopColor: useColorModeValue('white', 'gray.800'),
-        pos: 'absolute',
-        bottom: '-16px',
-        left: '50%',
-        transform: 'translateX(-50%)',
-      }}>
+      align={'center'}>
       {props.children}
     </Stack>
   );
@@ -79,7 +62,7 @@ const TestimonialAvatar = (props) => {
 
 export default function TestimonialList() {
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.700')} height={'500px'}>
+    <Box bg={useColorModeValue('gray.100', 'gray.700')} >
       <Container maxW={'7xl'} py={16} as={Stack} spacing={12}>
         <Stack spacing={0} align={'center'}>
           <Heading>Testimoni</Heading>
